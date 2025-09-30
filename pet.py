cc76ae11894936679e65e653984d54a4c4c2dcaf
Bartlomiej Niedziela <bartlomiej.niedziela@ing.com>
2025-09-30 22:25:27 +0200

    use edition 2 on session

--- pet.py
+++ pet.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, jsonify, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, MetaData, Sequence, String, Table, select, ForeignKey
+from sqlalchemy import Column, Integer, MetaData, Sequence, String, Table, select, ForeignKey, event
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import relationship
@@ -9,10 +9,6 @@ from typing import List
 
 class Base(DeclarativeBase):
   pass
-
-db = SQLAlchemy(model_class=Base)
-metadata_obj = MetaData()
-
 
 class Pet(Base):
     __tablename__ = "PET"
@@ -65,10 +61,28 @@ class Category(Base):
 
 
 # initialize the app with the extension
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "oracle+oracledb://app_service_user[app_schema]:SomePass4321@localhost:5521/?service_name=testpdb_service_v2"
-db.init_app(app)
 
+db = SQLAlchemy(model_class=Base)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "oracle+oracledb://app_service_user[app_schema]:SomePass4321@localhost:5521/?service_name=testpdb_service"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # Ensure the app context is active so db.engine is available
+    with app.app_context():
+        @event.listens_for(db.engine, "connect")
+        def alter_session_on_connect(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("ALTER SESSION SET EDITION = V2")
+            cursor.close()
+
+    return app
+
+
+app = create_app()
 
 @app.route('/pets', methods=["GET"])
 def all_pets():
@@ -120,3 +134,6 @@ def all_categories():
     if categories is None:
        return jsonify({"error": "No Category found"}), 404
     return jsonify([category.to_dict() for category in categories])
+
+if __name__ == "__main__":
+    app.run(debug=True)
